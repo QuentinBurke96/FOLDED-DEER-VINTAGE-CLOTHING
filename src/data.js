1e54f04730dee9298485965248b17cdd9feb559a
quentinburke96 <qeburke.qe@gmail.com>
2022-04-05 21:06:31 -0400

    FRONT

---
+++ src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://spy.com/wp-content/uploads/2021/04/vint.jpg",
+      title: "OVERSIZED COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://imgix.bustle.com/uploads/image/2021/3/29/8ad5c13f-6751-4014-a8ca-57d0048d6b3e-0325_vintagetees.jpg?w=653&fit=crop&crop=faces&auto=format%2Ccompress",
+      title: "FITS ALL",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.guim.co.uk/img/media/1e5f80d2a0d0e6895384165ea5e086c5f26c9f5e/694_214_4036_2422/master/4036.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=a66004d55c66b153aeb36e133b8c8463",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://ragstock.com/wp-content/uploads/vintage-sports-t-shirts.jpg",
+      title: "SHIRT STYLE!",
+      cat:"UNISEX"
+    },
+    {
+      id: 2,
+      img: "https://vip.nypost.com/wp-content/uploads/sites/2/2019/03/vintage-shirts-749.jpg?quality=90&strip=all",
+      title: "RAP Ts",
+      cat:"coat"
+    },
+    {
+      id: 3,
+      img: "https://images.urbndata.com/is/image/UrbanOutfitters/68466788_091_b?$medium$&qlt=80&fit=constrain",
+      title: "JACKETS",
+      cat:"jeans"
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
